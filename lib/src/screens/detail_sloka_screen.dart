264205b7b81a6bb7e666ab9bf89f78826d5537fc
Andndre <agungandre687@gmail.com>
2024-05-28 23:05:14 +0800

    future builder detail sloka

--- lib/src/screens/detail_sloka_screen.dart
+++ lib/src/screens/detail_sloka_screen.dart
@@ -5,45 +5,10 @@ import 'package:vedanta_frontend/src/providers/gita_provider.dart';
 import 'package:vedanta_frontend/src/widgets/gita_card_widget.dart';
 import 'package:vedanta_frontend/src/widgets/music_player_widget.dart';
 
-class DetailSlokaScreen extends StatefulWidget {
+class DetailSlokaScreen extends StatelessWidget {
   final int bab;
   final int sloka;
   const DetailSlokaScreen({super.key, required this.bab, required this.sloka});
-
-  @override
-  State<DetailSlokaScreen> createState() => _DetailSlokaScreenState();
-}
-
-class _DetailSlokaScreenState extends State<DetailSlokaScreen> {
-  Map _sloka = {};
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _getSloka();
-  }
-
-  Future<void> _getSloka() async {
-    final gitaProvider = Provider.of<GitaProvider>(context, listen: false);
-    final response = await gitaProvider.getGitaSloka(widget.bab, widget.sloka);
-    if (response['error'] == true) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(response['message']),
-          backgroundColor: Colors.red,
-        ),
-      );
-    } else {
-      print(response);
-      setState(() {
-        _sloka = response;
-      });
-    }
-    setState(() {
-      _isLoading = false;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,71 +18,94 @@ class _DetailSlokaScreenState extends State<DetailSlokaScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                GitaCardWidget(
-                  headerText: 'Sedang Membaca:',
-                  subHeaderText:
-                      'BAB ${_sloka['numberBab']}: SLOKA ${_sloka['number']}',
-                  text: _sloka['babTitle'] ?? '',
-                  withButton: false,
-                ),
-                const SizedBox(height: 20),
-                // Media Player Widget
-                if (_sloka['urlPelafalan'] != null)
-                  MusicPlayerWidget(url: _sloka['urlPelafalan'])
-                else
-                  const CircularProgressIndicator(),
-                const SizedBox(height: 20),
-                const Text(
-                  'Sloka:',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  _sloka['content'] ?? '',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    // fontFamily: 'NotoSansDevanagari',
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Terjemahan:',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  _sloka['translationIndo'] ?? '',
-                  style: const TextStyle(fontSize: 16),
-                ),
-
-                const SizedBox(height: 20),
-                const Text(
-                  'Makna:',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10),
-                MarkdownBody(
-                  data: _sloka['makna'] ?? '',
-                  styleSheet: MarkdownStyleSheet(
-                    p: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
+        child: FutureBuilder<Map>(
+          future: _getSloka(context, bab, sloka),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text('No data found.'),
+              );
+            } else {
+              final sloka = snapshot.data!;
+              return SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    GitaCardWidget(
+                      headerText: 'Sedang Membaca:',
+                      subHeaderText:
+                          'BAB ${sloka['numberBab']}: SLOKA ${sloka['number']}',
+                      text: sloka['babTitle'] ?? '',
+                      withButton: false,
                     ),
-                  ),
+                    const SizedBox(height: 20),
+                    if (sloka['urlPelafalan'] != null)
+                      MusicPlayerWidget(url: sloka['urlPelafalan'])
+                    else
+                      const CircularProgressIndicator(),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Sloka:',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      sloka['content'] ?? '',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Terjemahan:',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      sloka['translationIndo'] ?? '',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Makna:',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    MarkdownBody(
+                      data: sloka['makna'] ?? '',
+                      styleSheet: MarkdownStyleSheet(
+                        p: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              );
+            }
+          },
         ),
       ),
     );
+  }
+
+  Future<Map> _getSloka(BuildContext context, int bab, int sloka) async {
+    final gitaProvider = Provider.of<GitaProvider>(context, listen: false);
+    final response = await gitaProvider.getGitaSloka(bab, sloka);
+    if (response['error'] == true) {
+      throw Exception(response['message']);
+    } else {
+      return response;
+    }
   }
 }
