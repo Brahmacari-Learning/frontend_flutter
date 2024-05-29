8114aa568ec92623e43e3f0a71a27cde0a432539
Andndre <agungandre687@gmail.com>
2024-05-29 18:21:15 +0800

    alarm ui

--- lib/src/screens/alarm_create_screen.dart
+++ lib/src/screens/alarm_create_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:numberpicker/numberpicker.dart';
+import 'package:provider/provider.dart';
+import 'package:vedanta_frontend/src/screens/alarm_select_doa_screen.dart';
 
 class AlarmCreateScreen extends StatefulWidget {
   const AlarmCreateScreen({super.key});
@@ -11,6 +13,9 @@ class AlarmCreateScreen extends StatefulWidget {
 class _AlarmCreateScreenState extends State<AlarmCreateScreen> {
   int hour = 0;
   int minute = 0;
+  List<bool> isSelected = [false, false, false, false, false, false, false];
+  dynamic selectedDoa;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +29,16 @@ class _AlarmCreateScreenState extends State<AlarmCreateScreen> {
         backgroundColor: Colors.white,
         elevation: 0,
         iconTheme: const IconThemeData(
-          color: Colors.purple, // Warna pink untuk back button
+          color: Colors.purple, // Warna ungu untuk back button
         ),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Time Picker
               Container(
                 decoration: BoxDecoration(
                   border: Border.all(
@@ -50,7 +57,152 @@ class _AlarmCreateScreenState extends State<AlarmCreateScreen> {
                     _numPicker(false),
                   ],
                 ),
-              )
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Ulangi",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              ToggleButtons(
+                borderColor: Colors.grey,
+                selectedBorderColor: Colors.purple,
+                fillColor: Colors.purple.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(10),
+                selectedColor: Colors.purple,
+                color: Colors.grey,
+                isSelected: isSelected,
+                onPressed: (int index) {
+                  setState(() {
+                    isSelected[index] = !isSelected[index];
+                  });
+                },
+                constraints: BoxConstraints(
+                  minWidth: (MediaQuery.of(context).size.width - 49) / 7,
+                ),
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text("Sen"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text("Sel"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text("Rab"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text("Kam"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text("Jum"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text("Sab"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text("Min"),
+                  ),
+                ],
+              ), // Ulangi
+
+              const SizedBox(height: 40),
+              const Text(
+                "Pilih Doa",
+                style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+              ),
+
+              // Doa picker
+              InkWell(
+                onTap: () async {
+                  dynamic doaFromSelectPage = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return const AlarmSelectDoaScreen();
+                      },
+                    ),
+                  );
+                  if (doaFromSelectPage == null) return;
+                  setState(() {
+                    selectedDoa = doaFromSelectPage;
+                  });
+                },
+                customBorder: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        selectedDoa != null
+                            ? selectedDoa['title']
+                            : 'Pilih Doa',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Icon(
+                        Icons.arrow_right_outlined,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Save Button
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (selectedDoa == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Pilih Doa terlebih dahulu'),
+                        ),
+                      );
+                      return;
+                    }
+
+                    // await Provider.of<AlarmProvider>(context, listen: false)
+                    //     .setAlarm(
+                    //         hour: hour,
+                    //         minute: minute,
+                    //         doa: selectedDoa['title'],
+                    //         id: selectedDoa['id']);
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Jadwalkan',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -71,14 +223,19 @@ class _AlarmCreateScreenState extends State<AlarmCreateScreen> {
       selectedTextStyle: const TextStyle(
         color: Colors.purple,
         fontSize: 48,
+        fontWeight: FontWeight.w700,
       ),
       itemWidth: 90,
       itemHeight: 60,
       haptics: true,
-      onChanged: (val) => {
+      onChanged: (val) {
         setState(() {
-          isHour ? hour = val : minute = val;
-        })
+          if (isHour) {
+            hour = val;
+          } else {
+            minute = val;
+          }
+        });
       },
     );
   }
---
+++ lib/src/screens/alarm_select_doa_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlarmSelectDoaScreen extends StatefulWidget {
+  const AlarmSelectDoaScreen({super.key});
+
+  @override
+  State<AlarmSelectDoaScreen> createState() => _AlarmSelectDoaScreenState();
+}
+
+class _AlarmSelectDoaScreenState extends State<AlarmSelectDoaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar());
+  }
+}
