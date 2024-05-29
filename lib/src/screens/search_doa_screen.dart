f88e00756cf4d2a477a6a53bd0050a00382aabff
Andndre <agungandre687@gmail.com>
2024-05-29 10:26:38 +0800

    search doa

---
+++ lib/src/screens/search_doa_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:vedanta_frontend/src/screens/detail_doa_screen.dart';
+
+class SearchDoaScreen extends StatefulWidget {
+  final List<dynamic> doas;
+  const SearchDoaScreen({super.key, required this.doas});
+
+  @override
+  State<SearchDoaScreen> createState() => _SearchDoaScreenState();
+}
+
+class _SearchDoaScreenState extends State<SearchDoaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cari Doa'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: widget.doas.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      child: ListTile(
+                        title: Text(
+                          widget.doas[index]['title'] ?? '',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          widget.doas[index]['body'] ?? '',
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailDoaScreen(
+                                idDoa: widget.doas[index]['id'],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
